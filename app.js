62de381e7498a3ec68f424e7c808cd6400d3f5f8
YM-95 <yordan_manolov95@abv.bgl>
2022-08-11 00:25:19 +0300

    Rework the project

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 
-const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
-};
+// const route = (event) => {
+//     event = event || window.event;
+//     event.preventDefault();
+//     window.history.pushState({}, "", event.target.href);
+//     handleLocation();
+// };
 
-const routes = {
-    "/": "/components/home.html",
-    "/contact": "/components/contact.html",
+// const routes = {
+//     "/": "/components/home.html",
+//     "/contact": "/components/contact.html",
 
-};
+// };
 
-const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
-};
+// const handleLocation = async () => {
+//     const path = window.location.pathname;
+//     const route = routes[path] || routes[404];
+//     const html = await fetch(route).then((data) => data.text());
+//     document.getElementById("main-page").innerHTML = html;
+// };
 
-window.onpopstate = handleLocation;
-window.route = route;
+// window.onpopstate = handleLocation;
+// window.route = route;
 
-handleLocation();
+// handleLocation();
